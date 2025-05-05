591f18944490c4f8e0177521cbe705f00e2bca59
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2025-05-05 09:33:37 -0700

    implement multiple edit feature

--- plugin/registereditor.lua
+++ plugin/registereditor.lua
@@ -2,8 +2,8 @@ local internals = require("internals")
 
 local function setup_user_commands()
     vim.api.nvim_create_user_command("RegisterEdit", function(opts)
-        internals.open_editor_window(opts.args)
-    end, { nargs = 1 })
+        internals.open_all_windows(opts.args)
+    end, { nargs = "+" })
 end
 
 setup_user_commands()
